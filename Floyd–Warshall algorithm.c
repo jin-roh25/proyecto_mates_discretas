9f910d68042577d5e725b0bdad2c09aaf35f217a
Keyteer <114495829+Keyteer@users.noreply.github.com>
2022-11-24 15:23:39 -0300

    Add files via upload
    codigo Floyd warshall

---
+++ Floyd–Warshall algorithm.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <math.h>
+
+int **Floyd_Warshall_algorithm(int **w, int V)
+{
+
+	int **dis = malloc(sizeof(int *) * V);
+	for (int i = 0; i < V; i++)
+	{
+		dis[i] = malloc(sizeof(int) * V);
+		for (int j = 0; j < V; j++)
+			dis[i][j] = w[i][j];
+	}
+
+	for (int i = 0; i < V; i++)
+	{
+		for (int j = 0; j < V; j++)
+		{
+			for (int k = 0; k < V; k++)
+			{
+				if (dis[i][j] > dis[i][k] + dis[k][j])
+					dis[i][j] = dis[i][k] + dis[k][j];
+			}
+		}
+	}
+
+	return dis;
+}
