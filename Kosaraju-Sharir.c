4a52f5b54b945e15c124a84cedf03159fabf07d4
jin-roh25 <sebag2511@gmail.com>
2022-12-12 11:09:47 -0300

    Kosaraju-Sharir algorithm y Dijkstra

--- Kosaraju-Sharir.c
+++ Kosaraju-Sharir.c
@@ -1,15 +1,13 @@
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
 
+int t = 0;
 
-int vertices[2][numVertices];
-int arcos[numVertices][numVertices];
-int vertpasdos[numVertices];
-int compConexas[numVertices][numVertices];
+int **asignarVertices(int numVertices)
+{
 
-int **asignarVertices(int numVertices){
-
-    int **vertices = malloc(2*sizeof(int));
+    int **vertices = malloc(2 * sizeof(int));
 
     vertices[0] = malloc(numVertices * sizeof(int));
     vertices[1] = malloc(numVertices * sizeof(int));
@@ -22,30 +20,40 @@ int **asignarVertices(int numVertices){
     return vertices;
 }
 
-int **asignarArcos(int numVertices){
+int **iniciarVertPasados(int numVertices)
+{
 
-    int **arcos = malloc(numVertices*sizeof(int))
+    int **vertPasados = malloc(2 * sizeof(int));
 
+    vertPasados[0] = malloc(numVertices * sizeof(int));
+    vertPasados[1] = malloc(numVertices * sizeof(int));
+
+    return vertPasados;
+}
+
+int **asignarArcos(int numVertices)
+{
+    int **arcos = malloc(numVertices * sizeof(int));
     for (int i = 0; i < numVertices; i++)
     {
-        arcos[i] = malloc(numVertices * sizeof(int));
+        arcos[i] = malloc((numVertices) * sizeof(int));
     }
-
-    printf("Ingrese si existen arcos pasando por cada uno de los vertices desde el primero: \n");
+    printf("Ingrese si existen(1) arcos pasando por cada uno de los vertices desde el primero: \n");
     for (int i = 0; i < numVertices; i++)
     {
         for (int j = 0; j < numVertices; j++)
         {
-            scanf("%d", &arcos[j][i]);
+            scanf("%d", &arcos[i][j]);
         }
     }
 
     return arcos;
 }
 
-int **initListComConexas(int numVertices){
+int **initListComConexas(int numVertices)
+{
 
-    int **comConexas = malloc(numVertices*sizeof(int));
+    int **comConexas = malloc(numVertices * sizeof(int));
 
     for (int i = 0; i < numVertices; i++)
     {
@@ -56,7 +64,7 @@ int **initListComConexas(int numVertices){
     {
         for (int j = 0; j < numVertices; j++)
         {
-            compConexas[i][1] = 0;
+            comConexas[i][1] = 0;
         }
     }
 
@@ -67,46 +75,44 @@ void restablecerNoVisitado(int numVertices, int **vertices)
 {
     for (int i = 0; i < numVertices; i++)
     {
-        vertices[1][i]=0;
+        vertices[1][i] = 0;
     }
 }
 
-void visitar(int **vertices, int numVertices, int vertActual, int *vertPasados, int **arcos)
+void visitar(int **vertices, int numVertices, int vertActual, int **vertPasados, int **arcos)
 {
     if (vertices[1][vertActual] == 0)
     {
-        vertices[1][vertActual]==1;
+
+        vertices[1][vertActual] = 1;
 
         for (int i = 0; i < numVertices; i++)
         {
             if (arcos[vertActual][i] == 1)
             {
-                visitar(vertices, numVertices, vertices[0][i], vertPasados, arcos);
+                visitar(vertices, numVertices, i, vertPasados, arcos);
             }
-            
         }
-        
-        vertVisitados[t] = vertActual;
+
+        vertPasados[0][t] = vertActual;
         t++;
     }
-    
-
 }
 
-void asignar(int vertActual, int numCompomFuertConexa, int **vertices, int numVertices, int **arcos, int **compConexas)
+void asignar(int vertActual, int numCompomFuertConexa, int **vertPasados, int numVertices, int **arcos, int **compConexas)
 {
-    if (vertices[1][vertActual] == 0)
+    if (vertPasados[1][vertActual] == 0)
     {
-        vertices[1][vertActual] == 1;
+        vertPasados[1][vertActual] = 1;
 
-        compConexas[numCompomFuertConexa][t] = vertActual;
+        compConexas[numCompomFuertConexa][t] = vertPasados[0][vertActual];
         t++;
 
         for (int i = 0; i < numVertices; i++)
         {
             if (arcos[vertActual][i] == 1)
             {
-                asignar(vertices[0][i], numCompomFuertConexa, vertices, numVertices, arcos, compConexas);
+                asignar(i, numCompomFuertConexa, vertPasados, numVertices, arcos, compConexas);
             }
         }
     }
@@ -115,46 +121,44 @@ void asignar(int vertActual, int numCompomFuertConexa, int **vertices, int numVe
 int main(int argc, char const *argv[])
 {
     int numVertices;
-    int t = 0;
     int numCompomFuertConexa = 0;
 
     printf("Ingrese número de vertices: \n");
-    scanf("%d",&numVertices);
+    scanf("%d", &numVertices);
 
-    printf("a.1\n");
     int **vertices = asignarVertices(numVertices);
-    printf("a\n");
     int **arcos = asignarArcos(numVertices);
-    printf("b\n");
-    int *vertPasados = malloc(numVertices * sizeof(int))
-        printf("c\n");
+    int **vertPasados = iniciarVertPasados(numVertices);
 
     int **comConexas = initListComConexas(numVertices);
-    restablecerNoVisitado(numVertices,vertices);
-
+    restablecerNoVisitado(numVertices, vertices);
     for (int i = 0; i < numVertices; i++)
     {
-        visitar(vertices, numVertices,vertices[0][i], vertPasados, arcos);
+        visitar(vertices, numVertices, i, vertPasados, arcos);
     }
 
-    t=0;
+    t = 0;
     restablecerNoVisitado(numVertices, vertices);
 
     for (int i = 0; i < numVertices; i++)
     {
-        asignar(vertices[0][i], numCompomFuertConexa, vertices, numVertices, arcos, compConexas);
+
+        asignar(i, numCompomFuertConexa, vertPasados, numVertices, arcos, comConexas);
         numCompomFuertConexa++;
     }
-    
+
     for (int i = 0; i < numVertices; i++)
     {
-        for (int j = 0; j < count; j++)
+        printf("componete conexa n°%d: ", i + 1);
+        if (comConexas[i][0] != 0)
         {
-            if (compConexa[j][i]!=0)
+        }
+        for (int j = 0; j < numVertices; j++)
+        {
+            if (comConexas[i][j] != 0)
             {
-                printf("%d ", compConexa[j][i]);
+                printf("%d ", comConexas[i][j]);
             }
-            
         }
         printf("\n");
     }
